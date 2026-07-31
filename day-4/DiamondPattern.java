45b99c06e8bcf264145807fd5cbdb8c51cebe16e
poojeshkuppam <poojeshkuppam@gmail.com>
2026-07-31 16:19:30 +0530

    Added Diamond Pattern Program

---
+++ day-4/DiamondPattern.java
@@ -0,0 +1,31 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the diamond pattern
+
+        // Upper part of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}                                              
