26d6729bba9c09a892e1af53fe7661179ca6baa8
poojeshkuppam <poojeshkuppam@gmail.com>
2026-08-21 16:28:24 +0530

    updated

---
+++ ExceptionHandling/TryCatch.java
@@ -0,0 +1,17 @@
+public class TryCatch {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        }
+
+        System.out.println("Program continues...");
+    }
+}
